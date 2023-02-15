516c2ccb4ac33df1e2d29216a7ec4345e9cfbbb6
Yoo <santiagofrieza@gmail.com>
2023-02-15 12:10:52 -0500

    Router System implemented

---
+++ src/Router.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Nav from "./Components/NavBar";
+import Home from "./Components/Home";
+import Shop from "./Components/Shop";
+import {Cart} from "./Components/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+    <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
